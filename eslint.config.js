677616bea35e1b2bb055a87d08f9b1e125bebc61
Andrew Rackow <arackow@live.com>
2026-07-07 13:25:02 -0400

    configure eslint, prettier

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ const eslint = require('@eslint/js');
 const { defineConfig } = require('eslint/config');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
+const prettier = require('eslint-config-prettier');
 
 module.exports = defineConfig([
   {
@@ -35,7 +36,7 @@ module.exports = defineConfig([
   },
   {
     files: ['**/*.html'],
-    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
+    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility, prettier],
     rules: {},
   },
 ]);
